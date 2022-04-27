e9aa482bee5c7552362fcd254138e156431870cb
Sritaj <sritajp@gmail.com>
2022-04-28 01:21:21 +0530

    Added Springboot project for demonstrating jpa and hibernate with Spring Web, Lombok, H2, SpringData JPA, MySQL Driver dependencies

---
+++ src/main/java/in/sritaj/jpaandhibernate/JpaAndHibernateApplication.java
@@ -0,0 +1,13 @@
+package in.sritaj.jpaandhibernate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JpaAndHibernateApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JpaAndHibernateApplication.class, args);
+	}
+
+}
---
+++ src/test/java/in/sritaj/jpaandhibernate/JpaAndHibernateApplicationTests.java
@@ -0,0 +1,13 @@
+package in.sritaj.jpaandhibernate;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JpaAndHibernateApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
