a8482067f2a069fff9bfb2976b4b2261be8cff1f
Sritaj <sritajp@gmail.com>
2022-04-28 21:31:00 +0530

    Modified DataInitializer class to set data for HibernateAndJPA examples

--- src/main/java/in/sritaj/jpaandhibernate/bootstrap/DataInitializer.java
+++ src/main/java/in/sritaj/jpaandhibernate/bootstrap/DataInitializer.java
@@ -1,41 +1,27 @@
 package in.sritaj.jpaandhibernate.bootstrap;
 
 import in.sritaj.jpaandhibernate.entity.Person;
-import in.sritaj.jpaandhibernate.jdbc.PersonJdbcDao;
+import in.sritaj.jpaandhibernate.jpa.PersonJpaRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
-
-import java.util.Date;
-import java.util.List;
 
 @Component
 public class DataInitializer implements CommandLineRunner {
 
-    PersonJdbcDao personJdbcDao;
+    @Autowired
+    PersonJpaRepository personJpaRepositoy;
 
-    public DataInitializer(PersonJdbcDao personJdbcDao) {
-        this.personJdbcDao = personJdbcDao;
+    public DataInitializer(PersonJpaRepository personJpaRepositoy) {
+        this.personJpaRepositoy = personJpaRepositoy;
     }
 
     @Override
     public void run(String... args) throws Exception {
-        List<Person> personList = personJdbcDao.findAll();
 
-        for (Person p : personList) {
-            System.out.println(p);
-        }
+        Person personFromJPARepo = personJpaRepositoy.findById(10001);
+        System.out.println(personFromJPARepo);
 
-        Person person = personJdbcDao.findPersonById(10002);
-        System.out.println(person);
 
-        int rowAffected = personJdbcDao.deletePersonById(10001);
-        System.out.println(rowAffected);
-
-        int userCreated = personJdbcDao.insert(new Person(10003, "sp", "bengaluru", new Date()));
-        System.out.println(userCreated);
-
-        int userUpdated = personJdbcDao.update(new Person(10003, "anushaks", "nanjangud", new Date()));
-        System.out.println(userUpdated);
-        
     }
 }
