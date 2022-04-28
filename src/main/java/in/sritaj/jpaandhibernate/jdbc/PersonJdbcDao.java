81ff5787c6f795da60aa24043e65b5d57f5ab0ab
Sritaj <sritajp@gmail.com>
2022-04-28 10:44:36 +0530

    Added findByID method example

--- src/main/java/in/sritaj/jpaandhibernate/jdbc/PersonJdbcDao.java
+++ src/main/java/in/sritaj/jpaandhibernate/jdbc/PersonJdbcDao.java
@@ -17,4 +17,9 @@ public class PersonJdbcDao {
     public List<Person> findAll(){
         return jdbcTemplate.query("select * from person", new BeanPropertyRowMapper<Person>(Person.class));
     }
+
+    public Person findPersonById(int id){
+        return jdbcTemplate.queryForObject("select * from person where id=?", new Object[]{id},
+        new BeanPropertyRowMapper<Person>(Person.class));
+    }
 }
