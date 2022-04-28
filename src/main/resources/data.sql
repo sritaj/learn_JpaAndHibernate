dfc316161051db1f2a03e7738e1000112499a11c
Sritaj <sritajp@gmail.com>
2022-04-28 21:36:51 +0530

    Commented TABLE creation code, as Hibernate takes care on App run

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,10 +1,10 @@
-CREATE TABLE PERSON(
-     id integer not null,
-     name varchar(255) not null,
-     location varchar(255) not null,
-     birth_date timestamp,
-     primary key(id)
-);
+--CREATE TABLE PERSON(
+--     id integer not null,
+--     name varchar(255) not null,
+--     location varchar(255) not null,
+--     birth_date timestamp,
+--     primary key(id)
+--);
 
 Insert into PERSON(id, name, location, birth_date)
 values(10001, 'sritaj', 'odisha', sysdate());
