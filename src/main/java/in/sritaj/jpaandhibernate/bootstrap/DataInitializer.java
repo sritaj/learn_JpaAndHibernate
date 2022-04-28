c0e9a5d22541719f01dcdcbacb1cde70b21a9b6e
Sritaj <sritajp@gmail.com>
2022-04-28 11:26:38 +0530

    Added SpringJDBC examples for Insert and Update method

--- src/main/java/in/sritaj/jpaandhibernate/bootstrap/DataInitializer.java
+++ src/main/java/in/sritaj/jpaandhibernate/bootstrap/DataInitializer.java
@@ -5,6 +5,7 @@ import in.sritaj.jpaandhibernate.jdbc.PersonJdbcDao;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
 
+import java.util.Date;
 import java.util.List;
 
 @Component
@@ -30,5 +31,11 @@ public class DataInitializer implements CommandLineRunner {
         int rowAffected = personJdbcDao.deletePersonById(10001);
         System.out.println(rowAffected);
 
+        int userCreated = personJdbcDao.insert(new Person(10003, "sp", "bengaluru", new Date()));
+        System.out.println(userCreated);
+
+        int userUpdated = personJdbcDao.update(new Person(10003, "anushaks", "nanjangud", new Date()));
+        System.out.println(userUpdated);
+        
     }
 }
--- src/main/java/in/sritaj/jpaandhibernate/jdbc/PersonJdbcDao.java
+++ src/main/java/in/sritaj/jpaandhibernate/jdbc/PersonJdbcDao.java
@@ -6,6 +6,7 @@ import org.springframework.jdbc.core.BeanPropertyRowMapper;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Repository;
 
+import java.sql.Timestamp;
 import java.util.List;
 
 @Repository
@@ -25,5 +26,19 @@ public class PersonJdbcDao {
 
     public int deletePersonById(int id){
         return jdbcTemplate.update("delete from person where id=?", new Object[]{id});
+    }
+
+    public int insert(Person person){
+        return jdbcTemplate.update("Insert into PERSON(id, name, location, birth_date)\n" +
+                "values(?, ?, ?, ?)",
+                new Object[]
+                        {person.getId(), person.getName(), person.getLocation(), new Timestamp(person.getBirthdate().getTime())});
+    }
+
+    public int update(Person person){
+        return jdbcTemplate.update("update PERSON set name = ?, location =?, birth_date = ?"
+                +"where id =?",
+                new Object[]
+                        {person.getName(), person.getLocation(), new Timestamp(person.getBirthdate().getTime()), person.getId()});
     }
 }
