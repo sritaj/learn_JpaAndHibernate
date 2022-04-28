d74827f562685444e2c6b3882dce8c988bfb5eac
Sritaj <sritajp@gmail.com>
2022-04-29 00:02:29 +0530

    Added methods to insert, update and delete record for Person Entity

--- src/main/java/in/sritaj/jpaandhibernate/jpa/PersonJpaRepository.java
+++ src/main/java/in/sritaj/jpaandhibernate/jpa/PersonJpaRepository.java
@@ -6,6 +6,7 @@ import org.springframework.stereotype.Repository;
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
 import javax.transaction.Transactional;
+import java.util.Objects;
 
 /**
  * PersonJpaRepository class for implementing SQL queries/transactions for Person class using Hibernate and JPA
@@ -24,7 +25,49 @@ public class PersonJpaRepository {
      * @param id - specific id
      * @return Person - specified person
      */
-    public Person findById(int id){
+    public Person findById(int id) {
         return entityManager.find(Person.class, id);
     }
+
+    /**
+     * Method to create Person
+     *
+     * @param person - Person entity
+     * @return Person - created person entity
+     */
+    public Person insert(Person person) {
+        return entityManager.merge(person);
+    }
+
+    /**
+     * Method to update Person if present else create a new one
+     *
+     * @param id     - specific id
+     * @param person - Person entity
+     * @return String - message based on update of existing record or creating new record
+     */
+    public String update(int id, Person person) {
+        if (Objects.nonNull(findById(id))) {
+            entityManager.merge(person);
+            return "Successfully updated record";
+        }
+        entityManager.merge(person);
+        return "Existing record not found, created new record";
+    }
+
+    /**
+     * Method to update Person if present else create a new one
+     *
+     * @param id - specific id
+     * @return String - message based on deletion of existing record or not finding the record
+     */
+    public String deletePerson(int id) {
+        Person person = findById(id);
+        if (Objects.isNull(person)) {
+            return "Can't find Person    for ID " + id;
+        }
+        entityManager.remove(person);
+        return "Person removed";
+    }
+
 }
