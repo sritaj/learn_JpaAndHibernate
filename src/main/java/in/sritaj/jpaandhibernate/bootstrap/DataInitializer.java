81254feb25c6cb153dbbd425c735ba44822b1f0a
Sritaj <sritajp@gmail.com>
2022-04-28 10:26:28 +0530

    Added DataInitialiizer to initialise the execution commands

---
+++ src/main/java/in/sritaj/jpaandhibernate/bootstrap/DataInitializer.java
@@ -0,0 +1,28 @@
+package in.sritaj.jpaandhibernate.bootstrap;
+
+import in.sritaj.jpaandhibernate.entity.Person;
+import in.sritaj.jpaandhibernate.jdbc.PersonJdbcDao;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.stereotype.Component;
+
+import java.util.List;
+
+@Component
+public class DataInitializer implements CommandLineRunner {
+
+    PersonJdbcDao personJdbcDao;
+
+    public DataInitializer(PersonJdbcDao personJdbcDao) {
+        this.personJdbcDao = personJdbcDao;
+    }
+
+    @Override
+    public void run(String... args) throws Exception {
+        List<Person> personList = personJdbcDao.findAll();
+
+        for (Person p : personList) {
+            System.out.println(p);
+        }
+
+    }
+}
