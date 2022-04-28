446fff61670c9bb5ad59b798bc03194e4eeb1670
Sritaj <sritajp@gmail.com>
2022-04-29 00:03:15 +0530

    Added methods to validate insert,update,delete method from PersonJPARepo

--- src/main/java/in/sritaj/jpaandhibernate/bootstrap/DataInitializer.java
+++ src/main/java/in/sritaj/jpaandhibernate/bootstrap/DataInitializer.java
@@ -6,6 +6,11 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
 
+import java.util.Date;
+
+/**
+ * DataInitializer class for initializing data for running the Application
+ */
 @Component
 public class DataInitializer implements CommandLineRunner {
 
@@ -16,11 +21,34 @@ public class DataInitializer implements CommandLineRunner {
         this.personJpaRepositoy = personJpaRepositoy;
     }
 
+    /**
+     * Implemented Command Line Runner interface method for required JDBC data initialization on Application Run
+     *
+     * @param args - Args required
+     */
     @Override
     public void run(String... args) throws Exception {
 
         Person personFromJPARepo = personJpaRepositoy.findById(10001);
         System.out.println(personFromJPARepo);
+
+        Person newlyCreatedPerson = personJpaRepositoy.insert(new Person("Harry", "London", new Date()));
+        System.out.println(newlyCreatedPerson);
+        System.out.println(newlyCreatedPerson.getId());
+
+        newlyCreatedPerson.setName("Harry Potter");
+        String updatedPerson =  personJpaRepositoy.update(newlyCreatedPerson.getId(),newlyCreatedPerson);
+        System.out.println(updatedPerson);
+
+        String newlyCreatedPersonWhenIDDoesntExist = personJpaRepositoy.update(123456 , new Person("Ron", "London", new Date()));
+        System.out.println(newlyCreatedPersonWhenIDDoesntExist);
+
+        String personDeleteWhenIDDoesntExist = personJpaRepositoy.deletePerson(10393);
+        System.out.println(personDeleteWhenIDDoesntExist);
+
+        Person newlyCreatedPersonForDelete = personJpaRepositoy.insert(new Person("Malfoy", "London", new Date()));
+        String personDeleted = personJpaRepositoy.deletePerson(newlyCreatedPersonForDelete.getId());
+        System.out.println(personDeleted);
 
 
     }
