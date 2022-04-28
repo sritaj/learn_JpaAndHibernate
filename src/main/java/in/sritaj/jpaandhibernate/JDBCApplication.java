d534c67f94315081ff8948c8d73236888b3cc8cd
Sritaj <sritajp@gmail.com>
2022-04-28 21:29:31 +0530

    Standalone JDBCApplication class to demonstrate JDBC example r
    Implemented command line runner with necessary data initialisation to demonstrate JDBC examples
    SpringBootApplication annotation is commented out so that it won't interfere in the JPAandHibernate example runs

---
+++ src/main/java/in/sritaj/jpaandhibernate/JDBCApplication.java
@@ -0,0 +1,58 @@
+package in.sritaj.jpaandhibernate;
+
+import in.sritaj.jpaandhibernate.entity.Person;
+import in.sritaj.jpaandhibernate.jdbc.PersonJdbcDao;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import java.util.Date;
+import java.util.List;
+
+/**
+ * SpringBootApplication Initialisation for JDBCApplication
+ */
+//@SpringBootApplication
+public class JDBCApplication implements CommandLineRunner {
+
+    public JDBCApplication(PersonJdbcDao personJdbcDao) {
+        this.personJdbcDao = personJdbcDao;
+    }
+
+    public static void main(String[] args) {
+        SpringApplication.run(JDBCApplication.class, args);
+    }
+
+    @Autowired
+    public final PersonJdbcDao personJdbcDao;
+
+    /**
+     * Implemented Command Line Runner interface method for required JDBC data initialization on Application Run
+     *
+     * @param args - Args required
+     */
+    @Override
+    public void run(String... args) throws Exception {
+        List<Person> personList = personJdbcDao.findAll();
+
+        for (Person p : personList) {
+            System.out.println(p);
+        }
+
+        Person person = personJdbcDao.findPersonById(10002);
+        System.out.println(person);
+
+        int rowAffected = personJdbcDao.deletePersonById(10001);
+        System.out.println(rowAffected);
+
+        int userCreated = personJdbcDao.insert(new Person(10003, "sp", "bengaluru", new Date()));
+        System.out.println(userCreated);
+
+        int userUpdated = personJdbcDao.update(new Person(10003, "anushaks", "nanjangud", new Date()));
+        System.out.println(userUpdated);
+
+
+    }
+
+}
