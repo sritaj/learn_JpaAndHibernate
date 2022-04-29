1441c16ab79fe88f63f48dd700052b5957d12821
Sritaj <sritajp@gmail.com>
2022-04-29 11:09:12 +0530

    Added delete method for Course

--- src/main/java/in/sritaj/jpaandhibernate/repository/CourseRepository.java
+++ src/main/java/in/sritaj/jpaandhibernate/repository/CourseRepository.java
@@ -5,8 +5,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 
 import javax.persistence.EntityManager;
-import javax.persistence.PersistenceContext;
 import javax.transaction.Transactional;
+import java.util.Objects;
 
 /**
  * CourseRepository class for implementing SQL queries/transactions for Course class using Hibernate and JPA
@@ -25,7 +25,22 @@ public class CourseRepository {
      * @param id - specific id
      * @return Course - specified Course
      */
-    public Course findById(Long id){
+    public Course findById(Long id) {
         return entityManager.find(Course.class, id);
+    }
+
+    /**
+     * Method to delete Course if present
+     *
+     * @param id - specific id
+     * @return String - message based on deletion of existing record or not finding the record
+     */
+    public String deleteCourse(Long id) {
+        Course course = findById(id);
+        if (Objects.isNull(course)) {
+            return "Can't find Course for ID " + id;
+        }
+        entityManager.remove(course);
+        return "Course removed";
     }
 }
