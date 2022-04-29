f6f41fa8e9f792bb3bafabb9c1fd2f2137db1c21
Sritaj <sritajp@gmail.com>
2022-04-29 22:00:41 +0530

    Updated script for additional params

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -12,8 +12,8 @@ values(10001, 'sritaj', 'odisha', sysdate());
 Insert into PERSON(id, name, location, birth_date)
 values(10002, 'anusha', 'karnataka', sysdate());
 
-Insert into COURSE(id, course_name)
-values(18999, 'Geography')
+Insert into COURSE(id, course_name, created_date, last_updated_date)
+values(18999, 'Geography', sysdate(), sysdate());
 
 
 
