4cd8607b87109675454c24b07f73a1f8a20dce3e
Sritaj <sritajp@gmail.com>
2022-04-30 11:15:38 +0530

    Added sample data to load to the App for student, passport and review

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -8,12 +8,24 @@
 
 Insert into PERSON(id, name, location, birth_date)
 values(10001, 'sritaj', 'odisha', sysdate());
-
 Insert into PERSON(id, name, location, birth_date)
 values(10002, 'anusha', 'karnataka', sysdate());
 
 Insert into COURSE(id, course_name, created_date, last_updated_date)
-values(18999, 'Geography', sysdate(), sysdate());
+values(40999, 'Geography', sysdate(), sysdate());
+
+Insert into PASSPORT(id, passport_id)
+values(40000, 'E23SDK902');
+Insert into PASSPORT(id, passport_id)
+values(40001, 'E23SDK903');
+
+Insert into STUDENT(id, name, passport_id)
+values(20021, 'sritaj', 40000);
+Insert into STUDENT(id, name, passport_id)
+values(20022, 'anusha', 40001);
+
+Insert into REVIEW(id, rating, description)
+values(50001, 5, 'Wonderful Course');
 
 
 
