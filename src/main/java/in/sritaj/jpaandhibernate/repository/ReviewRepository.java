5dc1d2ba3fb41a08040df5398ccc18c58cc64eeb
Sritaj <sritajp@gmail.com>
2022-05-01 23:48:42 +0530

    Added Methods to retrieve course based on review & reviews based on course

--- src/main/java/in/sritaj/jpaandhibernate/repository/ReviewRepository.java
+++ src/main/java/in/sritaj/jpaandhibernate/repository/ReviewRepository.java
@@ -17,6 +17,11 @@ public class ReviewRepository {
     @Autowired
     EntityManager entityManager;
 
+    private final String queryForFetchingReviewsForSpecifiedCourse =
+            "select * from course \n" +
+                    "inner join review on course.id = review.course_id\n" +
+                    "where course_id = ?";
+
     /**
      * Method to fetch Passport based on id
      *
@@ -30,8 +35,8 @@ public class ReviewRepository {
     /**
      * Method to create Review for the Specified course
      *
-     * @param ID - specific Course ID
-     * @param rating - Rating for the specified course
+     * @param ID          - specific Course ID
+     * @param rating      - Rating for the specified course
      * @param description - Description for the course
      */
     public void createReviewForSpecifiedCourse(Long ID, Rating rating, String description) {
@@ -45,17 +50,40 @@ public class ReviewRepository {
     /**
      * Method to create Review for the Specified course
      *
-     * @param ID - specific Course ID
+     * @param ID         - specific Course ID
      * @param reviewList - List of Reviews
      */
     public void createMultipleReviewsForSpecifiedCourse(Long ID, List<Review> reviewList) {
         Course course = entityManager.find(Course.class, ID);
-        for(Review review : reviewList) {
+        for (Review review : reviewList) {
             review.setCourse(course);
             course.setReview(review);
             //new Review(review.getRating(), review.getDescription());
             entityManager.persist(review);
         }
 
+    }
+
+    //TODO - Method giving failure on Retreieval, SQLNativeQuery returning same review equivalent to no of diff Reviews present
+    /**
+     * Method to retrive Reviews based on the specified Course id
+     *
+     * @param ID - specific Course ID
+     * @return List - List of reviews
+     */
+    public List<Review> retrieveReviewsForSpecifiedCourse(Long ID) {
+        Course course = entityManager.find(Course.class, ID);
+        return course.getReviewList();
+    }
+
+    /**
+     * Method to retrive Course based on the Review id
+     *
+     * @param ID - specific Review ID
+     * @return Course - Matching Course
+     */
+    public Course retrieveCourseBasedOnTheReview(Long ID) {
+        Review review = entityManager.find(Review.class, ID);
+        return review.getCourse();
     }
 }
