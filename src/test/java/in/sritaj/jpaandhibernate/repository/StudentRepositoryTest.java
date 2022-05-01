27af42be3816b197bd9de2bb5d094be377bc920d
Sritaj <sritajp@gmail.com>
2022-05-01 10:41:14 +0530

    Added Unit Tests for StudentRepository methods

---
+++ src/test/java/in/sritaj/jpaandhibernate/repository/StudentRepositoryTest.java
@@ -0,0 +1,32 @@
+package in.sritaj.jpaandhibernate.repository;
+
+import com.github.javafaker.Faker;
+import in.sritaj.jpaandhibernate.entity.Passport;
+import in.sritaj.jpaandhibernate.entity.Student;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.testng.AbstractTestNGSpringContextTests;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+@SpringBootTest
+public class StudentRepositoryTest extends AbstractTestNGSpringContextTests {
+
+    @Autowired
+    StudentRepository studentRepository;
+
+    Faker fs = new Faker();
+
+    @Test(testName = "Validate creation of Student based with Passport Details")
+    public void validateStudentWithPassportDetails(){
+        Passport passport = new Passport(fs.idNumber().ssnValid());
+        String extractPassportID = passport.getPassportID();
+        Student student = new Student(fs.name().fullName());
+        studentRepository.saveStudentWithPassport(passport, student);
+        Long studentID = student.getId();
+
+        Student createdStudent = studentRepository.findById(studentID);
+        Assert.assertNotNull(createdStudent.getPassport());
+        Assert.assertEquals(createdStudent.getPassport().getPassportID(), extractPassportID);
+    }
+}
