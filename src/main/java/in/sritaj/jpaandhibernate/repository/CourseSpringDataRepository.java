5cef6d08e9c549855c0f813c71d0794bba4ea5ba
Sritaj <sritajp@gmail.com>
2022-05-03 00:48:59 +0530

    Added Spring Data Rest annotation and specified path

--- src/main/java/in/sritaj/jpaandhibernate/repository/CourseSpringDataRepository.java
+++ src/main/java/in/sritaj/jpaandhibernate/repository/CourseSpringDataRepository.java
@@ -2,9 +2,11 @@ package in.sritaj.jpaandhibernate.repository;
 
 import in.sritaj.jpaandhibernate.entity.Course;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
 import java.util.List;
 
+@RepositoryRestResource(path = "courses")
 public interface CourseSpringDataRepository extends JpaRepository<Course, Long> {
 
      List<Course> findByName(String name);
