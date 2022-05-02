e853618a2a4f51f3aeaaf9acb37d20a7c6c3b9ad
Sritaj <sritajp@gmail.com>
2022-05-03 00:31:48 +0530

    Added custom method to SpringJPA Course Repository

--- src/main/java/in/sritaj/jpaandhibernate/repository/CourseSpringDataRepository.java
+++ src/main/java/in/sritaj/jpaandhibernate/repository/CourseSpringDataRepository.java
@@ -3,6 +3,9 @@ package in.sritaj.jpaandhibernate.repository;
 import in.sritaj.jpaandhibernate.entity.Course;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface CourseSpringDataRepository extends JpaRepository<Course, Long> {
 
+     List<Course> findByName(String name);
 }
