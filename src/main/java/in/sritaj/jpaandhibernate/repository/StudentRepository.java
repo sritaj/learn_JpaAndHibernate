fe02c9ec839a8cdf4f833f81cb2736d832181ce7
Sritaj <sritajp@gmail.com>
2022-05-02 10:33:31 +0530

    Added method to retrieve student and course details

--- src/main/java/in/sritaj/jpaandhibernate/repository/StudentRepository.java
+++ src/main/java/in/sritaj/jpaandhibernate/repository/StudentRepository.java
@@ -1,5 +1,6 @@
 package in.sritaj.jpaandhibernate.repository;
 
+import in.sritaj.jpaandhibernate.entity.Course;
 import in.sritaj.jpaandhibernate.entity.Passport;
 import in.sritaj.jpaandhibernate.entity.Student;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -7,6 +8,9 @@ import org.springframework.stereotype.Repository;
 
 import javax.persistence.EntityManager;
 import javax.transaction.Transactional;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
 
 @Repository
 @Transactional
@@ -36,5 +40,23 @@ public class StudentRepository {
      */
     public Student findById(Long id) {
         return entityManager.find(Student.class, id);
+    }
+
+    /**
+     * Method to retrieve Student and Courses
+     *
+     * @param id - specific id
+     * @return Hashmap - returns Hashmap with Student Name as Key  and List Of Courses as Value
+     */
+    public HashMap<String, List<String>> retrieveStudentAndCourses(Long id) {
+        Student student = entityManager.find(Student.class, id);
+        List<Course> course = student.getCourseList();
+        List<String> titles = new ArrayList<>();
+        for (Course c : course) {
+            titles.add(c.getCourseName());
+        }
+        HashMap<String, List<String>> details = new HashMap<>();
+        details.put(student.getName(), titles);
+        return details;
     }
 }
