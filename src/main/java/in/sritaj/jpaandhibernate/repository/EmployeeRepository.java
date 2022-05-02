a675ea120ee1e4f4491173d6c92ab06b178f1d1d
Sritaj <sritajp@gmail.com>
2022-05-02 11:48:24 +0530

    Added method to insert and retrieve employee

---
+++ src/main/java/in/sritaj/jpaandhibernate/repository/EmployeeRepository.java
@@ -0,0 +1,35 @@
+package in.sritaj.jpaandhibernate.repository;
+
+import in.sritaj.jpaandhibernate.entity.Employee;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+
+import javax.persistence.EntityManager;
+import javax.transaction.Transactional;
+import java.util.List;
+
+@Repository
+@Transactional
+public class EmployeeRepository {
+
+    @Autowired
+    EntityManager entityManager;
+
+    /**
+     * Method to insert Employee
+     *
+     * @param employee - employee entity
+     */
+    public void insertEmployee(Employee employee) {
+        entityManager.persist(employee);
+    }
+
+    /**
+     * Method to retrieve List Of Employees
+     *
+     * @return List - list of employees
+     */
+    public List<Employee> retrieveEmployees() {
+        return entityManager.createQuery("Select E from Employee E", Employee.class).getResultList();
+    }
+}
