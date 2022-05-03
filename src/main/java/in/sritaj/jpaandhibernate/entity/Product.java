d5bc9507226af16a818181a80511cb3ec6872fcd
Sritaj <sritajp@gmail.com>
2022-05-03 18:11:11 +0530

    Added Product Entity class

---
+++ src/main/java/in/sritaj/jpaandhibernate/entity/Product.java
@@ -0,0 +1,31 @@
+package in.sritaj.jpaandhibernate.entity;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.*;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@Entity
+public class Product {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private int id;
+
+    @Column(nullable = false)
+    private String name;
+
+    private String description;
+
+    private Double price;
+
+    public Product(String name, String description, double price) {
+        this.name = name;
+        this.description = description;
+        this.price = price;
+    }
+}
