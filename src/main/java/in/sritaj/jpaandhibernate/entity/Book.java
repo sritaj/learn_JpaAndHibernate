02b0b4c29928313a73029008ac89e19ed7f90bb3
Sritaj <sritajp@gmail.com>
2022-05-04 00:19:46 +0530

    Added Book Entity and implemented ID Generation based on Table(custom)

---
+++ src/main/java/in/sritaj/jpaandhibernate/entity/Book.java
@@ -0,0 +1,29 @@
+package in.sritaj.jpaandhibernate.entity;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.*;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@Entity
+public class Book {
+
+    @TableGenerator(name = "employee_gen", table = "id_gen", pkColumnName = "gen_name", valueColumnName = "gen_val", allocationSize = 1)
+    @Id
+    @GeneratedValue(strategy = GenerationType.TABLE, generator = "employee_gen")
+    private Long ID;
+
+    private String title;
+    private String author;
+    private String publisher;
+
+    public Book(String title, String author, String publisher) {
+        this.title = title;
+        this.author = author;
+        this.publisher = publisher;
+    }
+}
