c076764e207c1292f8368c154f5552fb64c5da9f
Sritaj <sritajp@gmail.com>
2022-05-04 01:39:47 +0530

    Added methods for FindBy using diff clauses like -> IN, Contains, LIKE

--- src/main/java/in/sritaj/jpaandhibernate/repository/ProductSpringDataRepository.java
+++ src/main/java/in/sritaj/jpaandhibernate/repository/ProductSpringDataRepository.java
@@ -3,8 +3,25 @@ package in.sritaj.jpaandhibernate.repository;
 import in.sritaj.jpaandhibernate.entity.Product;
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.List;
+
 /**
  * ProductSpringDataRepository interface for extending CrudRepository for CRUD operations on Product Entity using SpringJPA
  */
 public interface ProductSpringDataRepository extends CrudRepository<Product, Integer> {
+
+    List<Product> findByName(String name);
+
+    List<Product> findByNameAndDescription(String name, String description);
+
+    List<Product> findByPriceGreaterThan(Double price);
+
+    List<Product> findByDescriptionContains(String description);
+
+    List<Product> findByPriceBetween(Double priceOne, Double priceTwo);
+
+    List<Product> findByNameLike(String name);
+
+    List<Product> findByIdIn(List<Integer> ids);
+
 }
