46328d24e42ae0972bd7e94114fd4e96712508ad
Sritaj <sritajp@gmail.com>
2022-05-03 11:49:34 +0530

    Updated Ratings

--- src/main/java/in/sritaj/jpaandhibernate/enums/Rating.java
+++ src/main/java/in/sritaj/jpaandhibernate/enums/Rating.java
@@ -4,12 +4,10 @@ package in.sritaj.jpaandhibernate.enums;
  * Rating ENUM to set Review for the specified Course
  */
 public enum Rating {
-    ONE(1),
-    TWO(2),
-    THREE(3),
-    FOUR(4),
-    FIVE(5);
+    ONE,
+    TWO,
+    THREE,
+    FOUR,
+    FIVE;
 
-    Rating(int i) {
-    }
 }
