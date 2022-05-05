322b1ee930836f30e85b45d07ebb44969a52b363
Sritaj <sritajp@gmail.com>
2022-05-05 13:04:26 +0530

    Added custom FindByIds method with Pagination

--- src/main/java/in/sritaj/jpaandhibernate/repository/ProductSpringDataRepositoryPaginationAndSorting.java
+++ src/main/java/in/sritaj/jpaandhibernate/repository/ProductSpringDataRepositoryPaginationAndSorting.java
@@ -1,7 +1,15 @@
 package in.sritaj.jpaandhibernate.repository;
 
 import in.sritaj.jpaandhibernate.entity.Product;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.repository.PagingAndSortingRepository;
 
+import java.util.List;
+
+/**
+ * ProductSpringDataRepository interface for extending PagingAndSortingRepository for Paging and Sorting operations on Product Entity using SpringJPA
+ */
 public interface ProductSpringDataRepositoryPaginationAndSorting extends PagingAndSortingRepository<Product, Integer> {
+
+    List<Product> findByIdIn(List<Integer> ids, Pageable pageable);
 }
