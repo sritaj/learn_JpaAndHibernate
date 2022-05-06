01b6dc0b4cedfc274524f766eea5db20812d8ead
Sritaj <sritajp@gmail.com>
2022-05-06 20:27:58 +0530

    Added Test to validate Crud operation for Payment and Sub Entities

---
+++ src/test/java/in/sritaj/jpaandhibernate/repository/PaymentSpringDataRepositoryTest.java
@@ -0,0 +1,46 @@
+package in.sritaj.jpaandhibernate.repository;
+
+import com.github.javafaker.Faker;
+import in.sritaj.jpaandhibernate.entity.inheritancemapping.singletable.Cheque;
+import in.sritaj.jpaandhibernate.entity.inheritancemapping.singletable.CreditCard;
+import in.sritaj.jpaandhibernate.entity.inheritancemapping.singletable.Payment;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.testng.AbstractTestNGSpringContextTests;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+import java.util.List;
+
+@SpringBootTest
+public class PaymentSpringDataRepositoryTest extends AbstractTestNGSpringContextTests {
+
+    @Autowired
+    private PaymentSpringDataRepository paymentSpringDataRepository;
+
+    private Faker fs = new Faker();
+
+    @Test(testName = "Validate Payment using Credit Card")
+    public void validatePaymentUsingCreditCard() {
+        paymentSpringDataRepository.deleteAll();
+        Payment cc = new CreditCard(237.99, fs.business().creditCardNumber());
+        paymentSpringDataRepository.save(cc);
+        List<Object[]> paymentMode = paymentSpringDataRepository.findAllPaymentNQ();
+        String actualPaymentType = paymentMode.get(0)[0].toString();
+
+        Assert.assertEquals(actualPaymentType, "CC");
+
+    }
+
+    @Test(testName = "Validate Payment using Cheque")
+    public void validatePaymentUsingCheque() {
+        paymentSpringDataRepository.deleteAll();
+        Payment cc = new Cheque(100.99, fs.business().creditCardNumber());
+        paymentSpringDataRepository.save(cc);
+        List<Object[]> paymentMode = paymentSpringDataRepository.findAllPaymentNQ();
+        String actualPaymentType = paymentMode.get(0)[0].toString();
+
+        Assert.assertEquals(actualPaymentType, "CH");
+
+    }
+}
