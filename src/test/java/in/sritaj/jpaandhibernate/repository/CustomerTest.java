9ccbf7d5d329ac8ef7ce7b41aeab722b089e07e5
Sritaj <sritajp@gmail.com>
2022-05-07 00:20:42 +0530

    Added Test to validate Customer entity

---
+++ src/test/java/in/sritaj/jpaandhibernate/repository/CustomerTest.java
@@ -0,0 +1,33 @@
+package in.sritaj.jpaandhibernate.repository;
+
+import com.github.javafaker.Faker;
+import in.sritaj.jpaandhibernate.entity.componentmapping.Customer;
+import in.sritaj.jpaandhibernate.entity.componentmapping.CustomerAddress;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.testng.AbstractTestNGSpringContextTests;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+@SpringBootTest
+public class CustomerTest extends AbstractTestNGSpringContextTests {
+
+    @Autowired
+    private CustomerSpringDataRepository customerSpringDataRepository;
+
+    private Faker fs = new Faker();
+
+    @Test
+    public void validateCustomerCreation() {
+        CustomerAddress customerAddress = new CustomerAddress(
+                fs.address().streetAddress(),
+                fs.address().city(),
+                fs.address().state(),
+                fs.address().zipCode(),
+                fs.address().country());
+        Customer newCustomer = customerSpringDataRepository.save(new Customer(fs.funnyName().name(), customerAddress));
+
+        Assert.assertNotNull(newCustomer);
+
+    }
+}
