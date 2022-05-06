1b5eff7b668271a989b18e864189e9214fcbe7fe
Sritaj <sritajp@gmail.com>
2022-05-06 20:27:32 +0530

    Added repository for SQL Crud operation & method to fetch payment_mode

---
+++ src/main/java/in/sritaj/jpaandhibernate/repository/PaymentSpringDataRepository.java
@@ -0,0 +1,17 @@
+package in.sritaj.jpaandhibernate.repository;
+
+import in.sritaj.jpaandhibernate.entity.inheritancemapping.singletable.Payment;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.CrudRepository;
+
+import java.util.List;
+
+/**
+ * PaymentSpringDataRepository interface for extending CrudRepository for CRUD operations on Product Entity using SpringJPA
+ */
+public interface PaymentSpringDataRepository extends CrudRepository<Payment, Integer> {
+
+    @Query(value = "select payment_mode from payment", nativeQuery = true)
+    List<Object[]> findAllPaymentNQ();
+
+}
