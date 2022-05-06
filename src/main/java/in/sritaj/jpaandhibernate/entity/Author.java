806c51d0890ef597bdb44c744f5213ab17c19b7d
Sritaj <sritajp@gmail.com>
2022-05-06 11:21:54 +0530

    Added Author Entity to demonstrate examples on JPQL

---
+++ src/main/java/in/sritaj/jpaandhibernate/entity/Author.java
@@ -0,0 +1,36 @@
+package in.sritaj.jpaandhibernate.entity;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.*;
+
+/**
+ * Author Entity defining the Table structure and relevant mappings with dependent entities
+ */
+@Getter
+@Setter
+@NoArgsConstructor
+@Entity
+public class Author {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private int ID;
+
+    @Column(name = "first_name")
+    private String firstName;
+
+    @Column(name = "last_name")
+    private String lastName;
+
+    @Column(name = "books_published")
+    private int booksPublished;
+
+    public Author(String firstName, String lastName, int booksPublished) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.booksPublished = booksPublished;
+    }
+}
