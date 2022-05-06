26caab0c025397aa7bf676cb1a95109bd6053912
Sritaj <sritajp@gmail.com>
2022-05-06 11:22:33 +0530

    Created AuthorSpringDataRepository to implement SQL CRUD/JPQL operations

---
+++ src/main/java/in/sritaj/jpaandhibernate/repository/AuthorSpringDataRepository.java
@@ -0,0 +1,38 @@
+package in.sritaj.jpaandhibernate.repository;
+
+import in.sritaj.jpaandhibernate.entity.Author;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+/**
+ * AuthorSpringDataRepository interface for extending CrudRepository for CRUD operations on Author Entity using SpringJPA
+ */
+public interface AuthorSpringDataRepository extends CrudRepository<Author, Integer> {
+
+    @Query("from Author")
+    List<Author> findAllAuthors();
+
+    @Query("Select firstName, lastName from Author A")
+    List<Author> findAllAuthorsPartialData();
+
+    @Query("from Author where firstName = :firstName")
+    List<Author> findAllAuthorsByFirstName(@Param("firstName") String firstName);
+
+    @Query("from Author where booksPublished > :min and booksPublished < :max")
+    List<Author> findAuthorsForGivenPublishedBooks(@Param("min") int min, @Param("max") int max);
+
+    @Modifying
+    @Transactional
+    @Query("delete from Author where firstName = :firstName")
+    void deleteAuthorByfirstName(@Param("firstName") String firstName);
+
+    @Query("from Author")
+    List<Author> findAllAuthors(Pageable pageable);
+
+}
