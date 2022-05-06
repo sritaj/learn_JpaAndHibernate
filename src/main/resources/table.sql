9206b81b10cedbe6aab56eba33996e2dd17b3110
Sritaj <sritajp@gmail.com>
2022-05-06 23:34:45 +0530

    Added Employee and Sub Tables sql commands for Table creation

--- src/main/resources/table.sql
+++ src/main/resources/table.sql
@@ -4,7 +4,7 @@ create table course (
     created_date timestamp,
     is_deleted boolean,
     last_updated_date timestamp,
-    primary key (id) 
+    primary key (id)
 );
 create table employee (
     employee_type varchar(31) not null,
@@ -16,13 +16,13 @@ create table full_time_employee (
     salary decimal(19, 2),
     id bigint auto_increment not null,
     primary key (id),
-    foreign key (id) references employee(id)
+    foreign key (id) references employee(id) ON DELETE CASCADE
 );
 create table part_time_employee (
     hourly_wage decimal(19, 2),
     id bigint auto_increment not null,
     primary key (id),
-    foreign key (id) references employee(id)
+    foreign key (id) references employee(id) ON DELETE CASCADE
 );
 create table passport (
     id bigint auto_increment not null,
@@ -44,7 +44,6 @@ create table review (
     primary key (id),
     foreign key (course_id) references course(id)
 );
-
 create table student (
     id bigint auto_increment not null,
     address_line varchar(255),
@@ -60,49 +59,41 @@ create table student_course (
     foreign key (student_id) references student(id),
     foreign key (course_id) references course(id)
 );
-
 create table product(
-id int PRIMARY KEY auto_increment not null,
-name varchar(20),
-description varchar(100),
-price decimal(8,3) 
+    id int PRIMARY KEY auto_increment not null,
+    name varchar(20),
+    description varchar(100),
+    price decimal(8, 3)
 );
-
 create table author(
-id int PRIMARY KEY AUTO_INCREMENT,
-first_name varchar(20),
-last_name varchar(20),
-books_published int
-)
-
-create table book(
-id bigint PRIMARY KEY,
-title varchar (200),
-author varchar (200),
-publisher varchar (200)
+    id int PRIMARY KEY AUTO_INCREMENT,
+    first_name varchar(20),
+    last_name varchar(20),
+    books_published int
+) create table book(
+    id bigint PRIMARY KEY,
+    title varchar (200),
+    author varchar (200),
+    publisher varchar (200)
 );
-
 create table id_gen(
-gen_name varchar(60) PRIMARY KEY,
-gen_val int(20)
+    gen_name varchar(60) PRIMARY KEY,
+    gen_val int(20)
 );
-
 create table payment(
-id int PRIMARY KEY,
-payment_mode varchar(2),
-amount decimal(8,3) ,
-card_number varchar(20),
-check_number varchar(20)
+    id int PRIMARY KEY,
+    payment_mode varchar(2),
+    amount decimal(8, 3),
+    card_number varchar(20),
+    check_number varchar(20)
 );
-
 create table bike(
-id int PRIMARY KEY,
-amount decimal(8,3),
-bike varchar(20)
+    id int PRIMARY KEY,
+    amount decimal(8, 3),
+    bike varchar(20)
 );
-
 create table car(
-id int PRIMARY KEY,
-amount decimal(8,3),
-car varchar(20)
+    id int PRIMARY KEY,
+    amount decimal(8, 3),
+    car varchar(20)
 );
