6f17eb1d82be7a2705ad204b9500a40f97661192
Sritaj <sritajp@gmail.com>
2022-05-07 07:31:12 +0530

    Added Repository for stored procedure executions

---
+++ src/main/java/in/sritaj/jpaandhibernate/repository/ActionFiguresSpringDataRepository.java
@@ -0,0 +1,19 @@
+package in.sritaj.jpaandhibernate.repository;
+
+import in.sritaj.jpaandhibernate.entity.storedprocedures.ActionFigures;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.CrudRepository;
+
+import java.util.List;
+
+public interface ActionFiguresSpringDataRepository extends CrudRepository<ActionFigures, Integer> {
+
+    @Query(value = "CALL GetAllActionFigures", nativeQuery = true)
+    List<ActionFigures> findAllActionFigures();
+
+    @Query(value = "CALL GetAllActionFiguresByPrice (:price_in)", nativeQuery = true)
+    List<ActionFigures> findAllActionFiguresByPrice(double price_in);
+
+    @Query(value = "CALL GetAllActionFiguresByPrice (:price_in)", nativeQuery = true)
+    int findAllActionFiguresCountByPrice(double price_in);
+}
