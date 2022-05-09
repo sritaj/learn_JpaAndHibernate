e64959d342c171d13dba1a1c5b601f313eddab78
Sritaj <sritajp@gmail.com>
2022-05-09 12:25:33 +0530

    Added Entity for checking CompositeKey

---
+++ src/main/java/in/sritaj/jpaandhibernate/entity/compositekeys/Staff.java
@@ -0,0 +1,28 @@
+package in.sritaj.jpaandhibernate.entity.compositekeys;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.IdClass;
+
+/**
+ * Staff Entity defining the Table structure and relevant mappings with dependent entities
+ */
+@Getter
+@Setter
+@NoArgsConstructor
+@Entity
+@IdClass(StaffID.class)
+public class Staff {
+
+    @Id
+    private int id;
+
+    @Id
+    private String email;
+
+    private String name;
+}
