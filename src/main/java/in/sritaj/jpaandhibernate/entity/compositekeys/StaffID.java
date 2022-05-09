99801b52e35ba71a1c1613052c8217994231afb8
Sritaj <sritajp@gmail.com>
2022-05-09 12:26:04 +0530

    Added Class to define the Composite Keys for Staff Entity

---
+++ src/main/java/in/sritaj/jpaandhibernate/entity/compositekeys/StaffID.java
@@ -0,0 +1,19 @@
+package in.sritaj.jpaandhibernate.entity.compositekeys;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import java.io.Serializable;
+
+/**
+ * StaffID class implementing Serializable interface for creating composite key for Staff
+ */
+@Getter
+@Setter
+public class StaffID implements Serializable {
+
+    private int id;
+
+    private String email;
+
+}
