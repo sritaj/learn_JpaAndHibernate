8d05308dc58d8ce1dc2bf479f5b73a3506aacb33
Sritaj <sritajp@gmail.com>
2022-05-09 11:26:48 +0530

    Updated JavaDoc

--- src/main/java/in/sritaj/jpaandhibernate/repository/fileshandling/ImageSpringDataRepository.java
+++ src/main/java/in/sritaj/jpaandhibernate/repository/fileshandling/ImageSpringDataRepository.java
@@ -3,5 +3,8 @@ package in.sritaj.jpaandhibernate.repository.fileshandling;
 import in.sritaj.jpaandhibernate.entity.fileshandling.Image;
 import org.springframework.data.repository.CrudRepository;
 
+/**
+ * ImageSpringDataRepository interface for extending CrudRepository for CRUD operations on Image Entity using SpringJPA
+ */
 public interface ImageSpringDataRepository extends CrudRepository<Image, Integer> {
 }
